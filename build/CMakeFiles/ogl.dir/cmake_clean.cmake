a16d18c506b414868b5401a8ddf12424e74e91ef
xzijoq <xzijoq@gmail.com>
2019-02-22 16:32:12 +0530

    emacs config added

---
+++ build/CMakeFiles/ogl.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ogl.dir/src/glad.cpp.o"
+  "CMakeFiles/ogl.dir/src/main.cpp.o"
+  "ogl.pdb"
+  "ogl"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ogl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
