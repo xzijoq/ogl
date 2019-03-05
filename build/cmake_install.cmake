fc67c32ccfadd6c97b9afb48a2ab24149fb3d309
xzijoq <xzijoq@gmail.com>
2019-03-05 15:40:06 +0530

    test

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/qq/cpp/programs/one
+# Install script for directory: /home/qq/cpp/ogl
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/qq/cpp/programs/one/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/qq/cpp/ogl/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
---
+++ run.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+echo $1
+
+if [[ $1 != f ]]
+then
+    echo "not reb"
+    cp ./build/compile_commands.json ./compile_commands.json 
+    cmake --build build
+    ./build/ogl
+fi
+
+if [[ $1  = f  || $2 = f ]]
+then
+    echo "rebuilding"
+    rm -r build
+    cmake -S./ -B build -G "Ninja"
+
+    cmake --build build
+    cp ./build/compile_commands.json ./compile_commands.json 
+    ./build/ogl
+
+fi
+
+if [[ $1 = git ]]
+then
+    git add .
+    git commit -m "test"
+    git push
+fi
+
+if [[ $1 = copy ]]
+then
+    sudo ../copy.sh
+fi
